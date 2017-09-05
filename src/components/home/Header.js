f76a55704fc8b84bbc9c77cfc9dc7ed93196767f
JOHN JACKSON ROCK <akzonrigg@gmail.com>
2017-09-05 08:53:56 -0700

    sr1

---
+++ src/components/home/Header.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+//import PropTypes from 'prop-types'
+import { CardContent } from 'material-ui/Card';
+import Typography from 'material-ui/Typography';
+import './Header.css';
+import './For.php';
+// import './Estilo.html';
+// import {
+//     Head,
+//     Title,
+//     div,
+//     p,
+//     input,
+//     script,
+//     form,
+//     body
+
+// } from 'react-router-dom'
+
+import './Sx.css';
+
+import {
+    Switch,
+    Route,
+    Link,
+    NavLink
+
+} from 'react-router-dom'
+
+
+class Header extends Component {
+	render
+	
+	()
+	 {
+        return (
+			
+            <CardContent>
+				
+                <Typography type="headline">
+				
+				LAB J&J	
+                    <nav>
+						<ul style={{ listStyleType: 'none', padding: 0 }}>
+                            <li><NavLink exact to="/" activeClassName="selected">Home</NavLink></li>
+                            <li><NavLink exact to="/abouts" activeClassName="selected">abouts</NavLink></li>
+                            <li><NavLink exact to="/one" activeClassName="selected">one</NavLink></li>
+                            <li><NavLink exact to="/counters" activeClassName="selected">Counter</NavLink></li>
+                            <li><NavLink exact to="/users" activeClassName="selected">users</NavLink></li>
+                            <li><NavLink exact to="/ecomms" activeClassName="selected">ecomms</NavLink></li>
+                        </ul>
+					</nav>
+
+                </Typography>
+				
+		
+		
+
+
+            </CardContent>
+        );
+    }
+}
+
+export default Header;
+
